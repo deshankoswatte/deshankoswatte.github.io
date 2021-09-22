c38be134243842f8d83638db82b8fa990ad7b2e1
deshankoswatte <dehami.deshan@gmail.com>
2021-09-22 06:39:46 +0000

    Deploying to gh-pages from @ deshankoswatte/deshankoswatte.github.io@4b1bf7d5c8961d8de44731293ca93b92742a5076 🚀

--- _next/static/YuXZ9Z63Rxkew-4CSz1vR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/965-6fac0ab58716bc1af806.js","static/chunks/pages/index-4bac769c00664bb8ba3e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
